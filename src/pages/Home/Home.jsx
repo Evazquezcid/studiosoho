8914d4a50fa84244f468b2487a3bebca0b54cf33
EV <ev@MacBook-Air-de-EV.local>
2022-11-16 19:27:01 +0100

    soho3

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,12 +6,9 @@ import './Home.scss'
 
 const Home = () => {
   return (
-     
-    
-   
-   <div className='home'>
+  <div className='home'>
     <section class="sectionhome">
-     <img  className='logohome'src="../../../assets/logosohocopia.png" alt="vivifoto"/>
+    <img  className='logohome'src="../../../assets/logonegro.png" alt="vivifoto"/>
     </section>
   <section class="text-block">
     <h2 className='h2home'>EL ESTUDIO</h2>
@@ -27,7 +24,7 @@ const Home = () => {
     <h2 className='tatuadoras'>ARTISTAS</h2>
     <ul >
     <li className='galeriatatuadoras'>
-     <a title="instavivi" href="https://www.instagram.com/vivianacalvotattoo/"><img src="../../../assets/vivifoto.png" alt="vivifoto"/></a>
+    <a title="instavivi" href="https://www.instagram.com/vivianacalvotattoo/"><img src="../../../assets/vivifoto.png" alt="vivifoto"/></a>
       <h1 className="nombrestatuadoras">Viviana Calvo</h1>
       </li>
       <li className='galeriatatuadoras'>
@@ -46,12 +43,11 @@ const Home = () => {
     <img className='reseñas' src="../../../assets/reseña2.png" alt="reseña2"/>
     <img className='reseñas' src="../../../assets/reseña3.png" alt="reseña3"/>
     </div>
+    <a href="https://www.google.com/maps/place/Soho+Tattoo+M%C3%A1laga/@36.7165312,-4.4226402,15z/data=!4m7!3m6!1s0x0:0x6c90669e2f4391c3!8m2!3d36.7165312!4d-4.4226402!9m1!1b1" className='boton'> + ⭐️</a>
+    
   </div>
   </div>
-   
-    
-    
-    
+
   )
 }
 
