1eeebc43eccd747c374ea3b6fb090798dc611578
EV <ev@MacBook-Air-de-EV.local>
2022-11-16 15:13:35 +0100

    soho2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,12 +14,17 @@ const Home = () => {
      <img  className='logohome'src="../../../assets/logosohocopia.png" alt="vivifoto"/>
     </section>
   <section class="text-block">
-    <h2 className='h2home'>EL ESTUDIO...</h2>
+    <h2 className='h2home'>EL ESTUDIO</h2>
+    <div className='maquina' >
+    {/* <img className='fotomaquina' src="../../../assets/maquinatattoo.png" alt="fotomaquinatattoo"/> */}
+    <img className='linea' src="../../../assets/lineatinta.png" alt="linea"/>
+    </div>
+    
     <p className='phome'>Soho Tattoo nace desde la ilusión y la pasión por el arte y el dibujo.
     Para nosotras lo mas importante es encontrar el equilibrio perfecto en la manera en la que quieres mostrar la idea en tu piel y como llevarla a cabo. Te esperamos en el barrio del Soho, en Málaga , cerca del casco antiguo. Si quires más información pincha el botón y rellena nuestro formulario.</p>
   </section>
   <section class="zonaartistas">
-    <h2 className='tatuadoras'>Nuestras tatuadoras...</h2>
+    <h2 className='tatuadoras'>ARTISTAS</h2>
     <ul >
     <li className='galeriatatuadoras'>
      <a title="instavivi" href="https://www.instagram.com/vivianacalvotattoo/"><img src="../../../assets/vivifoto.png" alt="vivifoto"/></a>
@@ -34,6 +39,14 @@ const Home = () => {
     
   </section>
   <button className='boton'> <Link to='/Form'>Contacta</Link></button>
+  <div>
+    <h1 className='librodevisitas'>LIBRO DE VISITAS</h1>
+    <div className='fotosreseñas'>
+    <img className='reseñas' src="../../../assets/reseña1.png" alt="reseña"/>
+    <img className='reseñas' src="../../../assets/reseña2.png" alt="reseña2"/>
+    <img className='reseñas' src="../../../assets/reseña3.png" alt="reseña3"/>
+    </div>
+  </div>
   </div>
    
     
